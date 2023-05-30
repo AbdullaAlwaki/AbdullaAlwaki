34df42412692fcac785bb80094add2b4ae40799a
Abdulla Alwaki <abdullaalwaki@icloud.com>
2023-05-30 21:04:02 +0200

    add api

--- index.js
+++ index.js
@@ -79,7 +79,7 @@ form.addEventListener("submit", (e) => {
     text,
   };
   try {
-    fetch("https://yalla-be.onrender.com/portfolio/contact", {
+    fetch("https://api.aha-store.com/portfolio/contact", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
